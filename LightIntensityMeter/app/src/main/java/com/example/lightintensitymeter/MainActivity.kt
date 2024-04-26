a8e36b821a078a702d6bba973b77da15ede8c4aa
enesvarol189 <104737357+enesvarol189@users.noreply.github.com>
2024-04-26 06:29:02 +0300

    Add files via upload

---
+++ LightIntensityMeter/app/src/main/java/com/example/lightintensitymeter/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.lightintensitymeter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lightintensitymeter.ui.theme.LightIntensityMeterTheme
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.compose.runtime.mutableStateOf
+
+class MainActivity : ComponentActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private var light: Sensor? = null
+    private var lightIntensity = mutableStateOf(0f)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        setContent {
+            LightIntensityMeterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    GetValue(lightIntensity.value)
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        light?.also { light ->
+            sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_LIGHT) {
+            lightIntensity.value = event.values[0]
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Handle changes in sensor accuracy here
+    }
+}
+
+@Composable
+fun GetValue(lightIntensity: Float) {
+    Text("Light Intensity: ${lightIntensity}")  // Use the state variable here
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LightIntensityMeterTheme {
+        Greeting("Android")
+    }
+}
